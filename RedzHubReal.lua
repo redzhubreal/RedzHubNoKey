1aa4fcfd4e1ef9cb292fbc1f2ee28cc61c0e4fb8
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-05-16 18:41:03 +0530

    Update RedzHubReal.lua

--- RedzHubReal.lua
+++ RedzHubReal.lua
@@ -1,4 +1,1 @@
---loadstring(game:HttpGet("https://pastefy.app/2WClwzpK/raw"))()
---loadstring(game:HttpGet("https://pastefy.app/tp0OIWuC/raw"))()
---loadstring(game:HttpGet("https://pastefy.app/Tv9vW9ww/raw"))()
-loadstring(game:HttpGet("https://pastefy.app/beGeNbwS/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/fMKs8qMZ/raw"))()
